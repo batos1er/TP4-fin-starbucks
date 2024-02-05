cbb852e207edc5826327795b3137c88d440dddb3
batos1er <bap.menet@gmail.com>
2024-02-05 14:57:08 +0100

    2.11 fini

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -8,11 +8,16 @@ import { Cart } from "./cart";
 import { CartCounter } from "./cart-counter";
 import Link from "next/link";
 import supabase from "../utils/supabase";
+import { createClientComponentClient, createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import getUser from "../utils/supabase";
 
 type Props = {};
 
-const Menu: FC<Props> = memo(function () {
+const Menu: FC<Props> = memo(async function () {
 
+  const supabase = createClientComponentClient();
+
+  const user = await getUser(supabase);
   // supabase.auth.onAuthStateChange((event, session) => {
   //   setTimeout(async () => {
   //     // await on other Supabase function here
@@ -29,35 +34,37 @@ const Menu: FC<Props> = memo(function () {
             </Button>
           </Link>
 
-          <Popover as="div" className="flex justify-end">
-            {({ open }) => (
-              <>
-                <Popover.Button as={Button} variant={"ghost"} className={"!rounded-full h-[44px] w-[44px] !p-0 flex justify-center items-center aspect-square relative text-3xl"}>
-                  {open 
-                    ? <X size={18} weight="regular" />
-                    : <ShoppingBag size={24} weight="regular" />}
+          {user &&
+            <Popover as="div" className="flex justify-end">
+              {({ open }) => (
+                <>
+                  <Popover.Button as={Button} variant={"ghost"} className={"!rounded-full h-[44px] w-[44px] !p-0 flex justify-center items-center aspect-square relative text-3xl"}>
+                    {open 
+                      ? <X size={18} weight="regular" />
+                      : <ShoppingBag size={24} weight="regular" />}
 
-                  <div className="aspect-square bg-brand text-white text-center text-xs absolute -right-1 -top-1 rounded-full flex items-center justify-center h-[20px] w-[20px]">
-                    <div><CartCounter /></div>
-                  </div>
-                </Popover.Button>
+                    <div className="aspect-square bg-brand text-white text-center text-xs absolute -right-1 -top-1 rounded-full flex items-center justify-center h-[20px] w-[20px]">
+                      <div><CartCounter /></div>
+                    </div>
+                  </Popover.Button>
 
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
-                >
-                  <Popover.Panel className="absolute left-0 sm:left-auto right-0 top-full z-10 mt-6 sm:w-full sm:max-w-sm">
-                    <Cart />
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute left-0 sm:left-auto right-0 top-full z-10 mt-6 sm:w-full sm:max-w-sm">
+                      <Cart />
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          }
         </div>
       }
     />
